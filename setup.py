8ae8d24d9dcc69e9f187a721e6fa9ff57b072ac6
James Vickery <dev@jamesvickery.net>
2018-06-13 23:54:26 +0100

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ety',
-    version='1.0.2',
+    version='1.1.0',
     description='find the etymological origins of a word',
     long_description=long_description,
     long_description_content_type='text/markdown',
