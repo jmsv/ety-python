0181086f72188a0a5cab1157588ac79c869dd6b7
Alex Ward <alxwrd@googlemail.com>
2018-06-17 17:50:31 +0100

    Add CLI tests (#37)
    * add a stdout_capture decorator
    * add tests for cli
    * override sys.stdout instead of sys.stdout.write
    * fix python2 unicode issues

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 import ety
 
 
@@ -9,6 +10,47 @@ def test_circular_etymology():
     """Test to avoid https://github.com/jmsv/ety-python/issues/20
     This method is run with a 10 second timeout (see Makefile test)"""
     ety.origins('software', recursive=True)
+
+
+def stdout_capture(func):
+    """Decorator to capture stdout during a test for testing the command line
+    If you need to actually print to stdout during a test, use ._print
+    """
+    sys_stdout = sys.stdout
+
+    class MockWriter(object):
+        _print = sys_stdout.write
+
+        def __init__(self):
+            self._value = u""
+
+        def write(self, message):
+            try:
+                self._value += message.decode("utf-8")
+            except AttributeError:  # Already decoded
+                self._value += message
+
+        @property
+        def value(self):
+            # Remove trailing new line so tests make more sense
+            return self._value.strip()
+
+        @property
+        def lines(self):
+            return len(self.value.split("\n"))
+
+    def wrapper(obj):
+        output = MockWriter()
+        # Override stdout.write with the mock
+        sys.stdout = output
+
+        # Pass the mock to the test
+        result = func(obj, output)
+
+        # Restore stdout
+        sys.stdout = sys_stdout
+        return result
+    return wrapper
 
 
 class TestEty(unittest.TestCase):
@@ -34,6 +76,89 @@ class TestEty(unittest.TestCase):
             ety.tree('aerodynamically')).split('\n')), 10)
         self.assertGreaterEqual(len(str(
             ety.tree('fabric')).split('\n')), 4)
+
+    @stdout_capture
+    def test_cli_no_args(self, output):
+        words = ["test"]
+        sys.argv = ["ety.py", "test"]
+
+        ety.cli()
+
+        origins = ety.origins("test")
+
+        expected_lines = len(words) + len(origins)
+
+        self.assertEqual(expected_lines, output.lines)
+
+    @stdout_capture
+    def test_cli_recursive(self, output):
+        words = ["test"]
+        sys.argv = ["ety.py", "-r"] + words
+
+        ety.cli()
+
+        origins = ety.origins("test", recursive=True)
+
+        expected_lines = len(words) + len(origins)
+
+        self.assertEqual(expected_lines, output.lines)
+
+    @stdout_capture
+    def test_cli_tree(self, output):
+        words = ["test"]
+        sys.argv = ["ety.py", "-t"] + words
+
+        ety.cli()
+
+        tree = ety.tree("test")
+        expected_lines = len(tree)
+
+        self.assertEqual(expected_lines, output.lines)
+
+    @stdout_capture
+    def test_cli_multiple_words(self, output):
+        words = ["test", "word"]
+        sys.argv = ["ety.py"] + words
+
+        ety.cli()
+
+        origins = [
+            origin for word in words
+            for origin in ety.origins(word)
+        ]
+
+        expected_lines = len(words) + len(origins) + len(words) - 1
+
+        self.assertEqual(expected_lines, output.lines)
+
+    @stdout_capture
+    def test_cli_multiple_words_recursive(self, output):
+        words = ["test", "word"]
+        sys.argv = ["ety.py", "-r"] + words
+
+        ety.cli()
+
+        origins = [
+            origin for word in words
+            for origin in ety.origins(word, recursive=True)
+        ]
+
+        expected_lines = len(words) + len(origins) + len(words) - 1
+
+        self.assertEqual(expected_lines, output.lines)
+
+    @stdout_capture
+    def test_cli_multiple_words_tree(self, output):
+        words = ["test", "word"]
+        sys.argv = ["ety.py", "-t"] + words
+
+        ety.cli()
+
+        trees = [ety.tree(word) for word in words]
+
+        expected_length = sum(len(tree) for tree in trees) + len(words) - 1
+
+        self.assertEqual(expected_length, output.lines)
 
 
 if __name__ == '__main__':
