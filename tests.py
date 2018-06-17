3ef5e4022ac5181dfc1fe7dcdfb4da263a76b490
Alex Ward <alxwrd@googlemail.com>
2018-06-17 14:58:50 +0100

    add a stdout_capture decorator

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 import ety
 
 
@@ -9,6 +10,44 @@ def test_circular_etymology():
     """Test to avoid https://github.com/jmsv/ety-python/issues/20
     This method is run with a 10 second timeout (see Makefile test)"""
     ety.origins('software', recursive=True)
+
+
+def stdout_capture(func):
+    """Decorator to capture stdout during a test for testing the command line
+    If you need to actually print to stdout during a test, use ._print
+    """
+    sys_stdout_write = sys.stdout.write
+
+    class MockWriter(object):
+        _print = sys_stdout_write
+
+        def __init__(self):
+            self._value = u""
+
+        def __call__(self, message):
+            self._value += message
+
+        @property
+        def value(self):
+            # Remove trailing new line so tests make more sense
+            return self._value.strip()
+
+        @property
+        def lines(self):
+            return len(self.value.split("\n"))
+
+    def wrapper(obj):
+        output = MockWriter()
+        # Override stdout.write with the mock
+        sys.stdout.write = output
+
+        # Pass the mock to the test
+        result = func(obj, output)
+
+        # Restore stdout
+        sys.stdout.write = sys_stdout_write
+        return result
+    return wrapper
 
 
 class TestEty(unittest.TestCase):
