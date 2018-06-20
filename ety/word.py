da372a4bc5436f87136a27b71ecc1ccdbd6c9c7e
Alex Ward <alxwrd@googlemail.com>
2018-06-20 11:14:22 +0100

    fix case sensitivity issues

--- ety/word.py
+++ ety/word.py
@@ -21,7 +21,8 @@ class Word(object):
             'direct': [],
             'recursive': []
         }
-        self._id = u"{}:{}".format(self.word, self.language.iso)
+        self._id = u"{}:{}".format(
+            self.word.lower(), self.language.iso.lower())
 
     def origins(self, recursive=False):
         search = 'recursive' if recursive else 'direct'
@@ -32,8 +33,10 @@ class Word(object):
             return o
 
         row = list(filter(
-            lambda entry: entry['a_word'] == self.word and entry[
-                'a_lang'] == self.language.iso, etymwn_data))
+            lambda entry: (
+                entry['a_word'].lower() == self.word.lower()
+                and entry['a_lang'].lower() == self.language.iso.lower()),
+            etymwn_data))
 
         o = [Word(item['b_word'], item['b_lang']) for item in row]
 
