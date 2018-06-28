1575db87232db91a54fe8b6d1dfcdbd6d1c2b273
Alex Ward <alxwrd@googlemail.com>
2018-06-28 09:25:43 +0100

    add python 3.7 to .travis.yml and setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
     keywords='etymology origins english language words',
