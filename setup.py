b646ed906cbe2a4c1f5473d6b00c0ef217aa89cd
James Vickery <dev@jmsv.me>
2022-07-31 20:53:21 +0100

    update setup

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jmsv/ety-python",
     author="James Vickery",
-    author_email="dev@jamesvickery.net",
+    author_email="dev@jmsv.me",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
